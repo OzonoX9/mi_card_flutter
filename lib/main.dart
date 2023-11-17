4be291c1452b64535fa6bede14436f718e0edcef
Jin Ozono <ozononicholas@gmail.com>
2023-11-16 21:11:33 -0300

    project completed

--- lib/main.dart
+++ lib/main.dart
@@ -10,12 +10,82 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       home: Scaffold(
         backgroundColor: Colors.teal,
         body: SafeArea(
-            child: Column(
-          children: [],
-        )),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              CircleAvatar(
+                backgroundImage: AssetImage('lib/images/gojo_icon.jpeg'),
+                radius: 50,
+              ),
+              const Text(
+                'Jin Ozono',
+                style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 40,
+                  fontWeight: FontWeight.bold,
+                  fontFamily: 'Cinzel',
+                ),
+              ),
+              Text(
+                'FLUTTER DEVELOPER',
+                style: TextStyle(
+                  color: Colors.teal[100],
+                  fontSize: 17,
+                  fontWeight: FontWeight.bold,
+                  fontFamily: 'JosefinSans',
+                  letterSpacing: 2,
+                ),
+              ),
+              SizedBox(
+                height: 8,
+                width: 150,
+                child: Divider(
+                  color: Colors.teal[100],
+                ),
+              ),
+              Card(
+                color: Colors.white,
+                margin: EdgeInsets.symmetric(vertical: 10, horizontal: 25),
+                child: ListTile(
+                  leading: Icon(
+                    Icons.phone,
+                    color: Colors.teal[900],
+                  ),
+                  title: Text(
+                    '+595 985 123 456',
+                    style: TextStyle(
+                        color: Colors.teal[900],
+                        fontSize: 20,
+                        fontFamily: 'SourceSans3',
+                        fontWeight: FontWeight.bold),
+                  ),
+                ),
+              ),
+              Card(
+                color: Colors.white,
+                margin: EdgeInsets.symmetric(vertical: 10, horizontal: 25),
+                child: ListTile(
+                  leading: Icon(
+                    Icons.email,
+                    color: Colors.teal[900],
+                  ),
+                  title: Text(
+                    'jinozono@gmail.com',
+                    style: TextStyle(
+                        color: Colors.teal[900],
+                        fontSize: 20,
+                        fontFamily: 'SourceSans3',
+                        fontWeight: FontWeight.bold),
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
       ),
     );
   }
